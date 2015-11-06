13732d6d51fe5fd368dec2f22f27ae52f18d35f8
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-11-06 12:32:15 +0200

    muokattu kontrollereita ja näkymiä sekä tehty muutama suunnitelma sovelluksen näkymistä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,32 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        //echo 'Hello World!';
+        View::make('helloworld.html');
     }
-  }
+
+    public static function recipe_list() {
+        View::make('suunnitelmat/recipe_list.html');
+    }
+
+    public static function recipe_show() {
+        View::make('suunnitelmat/recipe_show.html');
+    }
+
+    public static function recipe_edit() {
+        View::make('suunnitelmat/recipe_edit.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/recipe', function() {
+    HelloWorldController::recipe_list();
+});
+
+$routes->get('/recipe/1', function() {
+    HelloWorldController::recipe_show();
+});
+
+$routes->get('/recipe/2', function() {
+    HelloWorldController::recipe_edit();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});

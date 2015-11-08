6911e51682d3191b9b049230a95851c337598a39
Elina <ehaverin@dhcp-asv-82.helsinki.fi>
2015-11-08 20:59:32 +0200

    muokattu näkymiä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,7 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-        echo 'Tämä on etusivu!';
+        View::make('home.html');
     }
 
     public static function sandbox() {
